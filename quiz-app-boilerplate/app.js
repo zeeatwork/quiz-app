fdb2216c10f97e1cd294331a088eeb71b6df6f9d
Zenzi Ali <ms_zenzi@yahoo.com>
2020-07-16 14:18:04 -0400

    Add files via upload

---
+++ quiz-app-boilerplate/app.js
@@ -0,0 +1,90 @@
+/**
+ * Example store structure
+ */
+'use strict';
+//MY ANSWER KEY
+const store = {
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'How do you create an alert box?',
+      answers: [
+        'msgBox(“Correct!”)',
+        'alertBox(“Correct!”)',
+        'alert(“Correct!”)',
+        'msg.write(“Correct!’)'
+      ],
+      correctAnswer: 'alert(“Correct!”)'
+    },
+    {
+      question: 'How do you call a function in Javascript?',
+      answers: [
+        'call.myFunction()',
+        'myFunction()',
+        'call myFunction()',
+        'callFunction=myFunction()'
+      ],
+      correctAnswer: 'myFunction()'
+    },
+    {
+      question: 'Inside what element do we place JavaScript?',
+      answers: [
+        '<scripting>',
+        '<js>',
+        '<javascript>',
+        '<script>'
+      ],
+      correctAnswer: '<script>'
+    },
+    {
+      question: 'How do you write an IF statement?',
+      answers: [
+        'if i = 7',
+        'if (i == 7)',
+        'if i ==7 then',
+        'if i = 7 then'
+      ],
+      correctAnswer: 'if (i == 7)'
+    },
+    {
+      question: 'How do you create a function?',
+      answers: [
+        'function: myFunction()',
+        'function = myFunction()',
+        'function myFunction()',
+        'myFunction <= function'
+      ],
+      correctAnswer: 'function myFunction()'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
+
+/**
+ * 
+ * Technical requirements:
+ * 
+ * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * See your course material, consult your instructor, and reference the slides for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ * 
+ */
+
+/********** TEMPLATE GENERATION FUNCTIONS **********/
+
+// These functions return HTML templates
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+/********** EVENT HANDLER FUNCTIONS **********/
+
+// These functions handle events (submit, click, etc)
